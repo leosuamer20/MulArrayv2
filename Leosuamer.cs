493e447c9bdf9cd55f22350d025a868bee71fe18
leosuamer20 <122435265+leosuamer20@users.noreply.github.com>
2023-01-12 14:04:34 +0800

    Create Leosuamer.cs

---
+++ Leosuamer.cs
@@ -0,0 +1,125 @@
+Guess if you know me game!
+=== created by John Leo D. Suamer==
+
+===========================
+
+
+Please enter your name
+
+Direction: Please select your mode easy ,hard ,difficult Enter Mode:
+easy mode 15 questions
+1.Who is my name?
+a.John b.John Leo c. John mark d.John david
+answer
+2.what is my age?
+a.18 b.19 c.20 d.21
+answer
+3.what is the date of my Birthday?
+a.Jan 20,2002 b.Feb 20,2002 c.March 20,2002 d. June 20,2002
+answer
+4.What is my middlename?
+a.Debelen b. arriola c. raniola d. Mabato
+answer
+
+5.What is my first name?
+a.John b.John Leo c.John Cruz d. June
+answer
+6.Where is my address?
+a.Pangi b.Bangbang c.Masiga d. Bachao 
+answer
+
+7.What is my facorite color?
+a.Red b. Blue c.white d.Orange
+answer
+8. Whart is my color of my hair?
+a.Brown b.Black C. White d.Gray
+answer
+9.What is my favorite biscuit
+a. Hansel b.Fita c. Combi d.Rebisco
+answer
+10. who is my father
+a.Christopher b. Rogelio c.Conrad d.Elmer?
+answer
+11 what is my age of my father ?
+a.48 b.49 c.50 d.51
+
+12. what is my favorite food of my father?
+a.Pusit b. Fish c.Dinuguan d.menudo
+answer
+13.Who is my mother?
+a. Teressa b.agnes c.Marissa d . Charo
+answer
+14. What is the age of my mother ?
+a.48 b.49 c.50 d.51
+answer
+15. What is my color of my t shirts
+a.Black b. gray c. greem d. blue
+answer
+continue
+hard mode
+Educational Background 15 Questions
+1.Where i enterd in grade 1
+ Bangbang, Pangi, Cabugao , Mangiliol
+answer
+2.What is my favorite subject in grade 1
+Math, Filipino ,Science , Mother Tounge
+answer
+3.What is my grade of mother tounge
+78 ,80 ,89 , 90
+answer
+4. Wher is the place of my school in i enetred
+Pangi, Bangbang, Central,Bognuyan
+answer
+5.Where i enetered in grade 2
+Pangi,Bangbang ,Central ,Bognuyan
+answer
+6.What is my favorite subject in i was grade 2 level
+ English ,Filipino,  Math, hks
+answer
+7. What is my grade in hks
+79 ,80 ,86 ,89
+answer
+8.where i entered in grade 7
+Bognuyan, Central ,Bangbang, MNHs
+answer
+9.What is my grade in ap subject
+88,89,90 91
+answer
+10.Where i entered in grade 9
+Bangbang ,Masiga ,Central, Masiga
+answer
+11.What is my favorite subject in grade 9
+Mapeh,Filipino, Math ,English
+answer
+12.What is course in grade 11 level
+stem, tvl ,housekkeping ,cookery
+answer
+13.what is my favorite subject in grade 11
+gen math ,pre calculus,filipino,chemistry
+answer
+14.What is my course on grade 12
+Stem ,cookery Housekeeping, Tvl
+answer
+15. what is my section on grade 11 and 12  
+A. B
+a8.How much i spend my time in my hobbies 
+
+answer
+9.What hobnies  i prefer everyday i play 
+
+answer
+10.What kind of things game the most value of me
+
+answer
+11. what is my solo hobbies
+answer
+12.What is my favorite online games character 
+answer
+13.Do I have any hobbies yes or no
+answer
+14. Can you give at least one sport that i can play
+
+answer
+What is the most favorite hobbies in my life
+
+answer
